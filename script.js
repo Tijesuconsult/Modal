c0b66d9db0993ab7707588553b919fb9e6aa4688
Tijesuconsult <ayoolablessing54@gmail.com>
2023-11-07 14:33:35 +0000

    Done

---
+++ script.js
@@ -0,0 +1,11 @@
+let mod = document.getElementById('modal')
+function clickme(){
+ mod.style.display='block'
+//  document.getElementById('modal').style.display='block'
+}
+function clickyou(){
+    mod.style.display='none'
+}
+function clickclose(){
+    mod.style.display='none'
+}
